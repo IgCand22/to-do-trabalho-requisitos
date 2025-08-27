be2ce81aba2746229c8bd129111b8789eb211384
Fernando Carvalho <folicos1fawkes@gmail.com>
2025-08-27 20:51:58 -0300

    Feito função para newButton, teste caso input nulo, pequenos acertos

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('button')
     const campo = document.getElementById('campo')
+    let cont = 1
 
     button.addEventListener('click', () => {
-        const item1 = document.getElementById('item1')
+        const item = document.getElementById('item')
+        if(item.value.trim() === "") return;
         const aFazer = document.createElement('div')
-        aFazer.id = 'aFazer'
+        aFazer.id = `aFazer${cont}`
         const textAFazer = document.createElement('p')
+        textAFazer.id = `text${cont}`
         const newButton = document.createElement('button')
-        newButton.textContent = '/'
-        textAFazer.textContent = item1.value;
+        newButton.textContent = '✓ Concluir'
+        newButton.id = `button${cont}`
+        textAFazer.textContent = item.value;
         aFazer.appendChild(textAFazer)
         aFazer.appendChild(newButton)
         campo.appendChild(aFazer)
+        cont++;
+        item.value = "";
 
         newButton.addEventListener('click', () => {
-
+            textAFazer.style.color = '#d4edda'
         })
     })
 
